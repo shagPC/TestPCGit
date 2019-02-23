4b52f9fda8a7d54b5868145e512738afe1f97fbf
shagPC <47909463+shagPC@users.noreply.github.com>
2019-02-23 10:16:22 +0600

    Add files via upload

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private ChromeDriver chrome;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("http://mail.ru");
+            chrome.FindElement(By.Id("q")).SendKeys("Зачем нужны автотесты");
+            chrome.FindElement(By.Id("q")).SendKeys(Keys.Enter);
+            chrome.Quit();
+        }
+    }
+}
